da8e0c8c3079950c738170d2545a9a68203b02e1
samyumobiMSIT <samyumobi@msitprogram.net>
2018-09-27 19:07:40 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- practice/M4/4.2 Evaluate Postfix/Solution.java
+++ practice/M4/4.2 Evaluate Postfix/Solution.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 
-public class Postfix {
+public class Solution {
     
     public static void main(String [] args) {
         String postfix = readInput();
