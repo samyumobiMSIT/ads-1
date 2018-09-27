f4d399b6ac471ceec642efad7804d1d910d12bb4
samyumobiMSIT <samyumobi@msitprogram.net>
2018-09-27 12:00:50 +0530

    Changes to pgm

--- assignments/m4/4.1 Balanced Paranthesis/Solution.java
+++ assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -1,50 +1,6 @@
-import java.util.Stack;
 public class Solution{
-	 private static final char LEFT_PAREN     = '(';
-    private static final char RIGHT_PAREN    = ')';
-    private static final char LEFT_BRACE     = '{';
-    private static final char RIGHT_BRACE    = '}';
-    private static final char LEFT_BRACKET   = '[';
-    private static final char RIGHT_BRACKET  = ']';
-
-    public static Boolean isBalanced(String s){
-    	Stack<Character> Stack = new Stack<Character>();
-    	for(int i=0; i<s.length();i++){
-    		if(s.charAt(i) == LEFT_PAREN)
-    			Stack.push(LEFT_PAREN);
-    		if(s.charAt(i)== RIGHT_PAREN)
-    			Stack.push(LEFT_BRACE);
-    		if(s.charAt(i) == LEFT_BRACKET)
-    			Stack.push(LEFT_BRACKET);
-
-    		if(s.charAt(i)== RIGHT_PAREN){
-    			if(Stack.isEmpty())
-    				return false;
-    			if(Stack.pop()!= LEFT_PAREN)
-    				return false;
-    		}
-    		else if(s.charAt(i) == RIGHT_BRACE){
-    			if(Stack.isEmpty())
-    				return false;
-    			if(Stack.pop()!= LEFT_BRACE)
-    				return false;
-    		}
-    		else if(s.charAt(i) == RIGHT_BRACKET){
-    			if(Stack.isEmpty())
-    				return false;
-    			if(Stack.pop()!= LEFT_BRACKET)
-    				return false;
-    		}
-    	}
-    	return Stack.isEmpty();
-    }
-
-public static void public static void main(String[] args) {
-		String beTrue = "[()]{}[{[()()]()}]";
-        String beFalse = "{[(])}";
-        System.out.println("True? " + balancedParentheses(beTrue));
-        System.out.println("False? " + balancedParentheses(beFalse));
-	
-}
-
+	public static void main(String[] a){
+		String exp = readInput();
+		System.out.println("Valid: "+validate(expr));
+	}
 }
