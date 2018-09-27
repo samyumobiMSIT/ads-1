daa4c63311e88cb9bd93f8e674815cfe40fe58d2
samyumobiMSIT <samyumobi@msitprogram.net>
2018-09-27 11:20:29 +0530

    m4-a1-1

--- assignments/m4/4.1 Balanced Paranthesis/Solution.java
+++ assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -6,4 +6,36 @@ public class Solution{
     private static final char LEFT_BRACKET   = '[';
     private static final char RIGHT_BRACKET  = ']';
 
+    public static Boolean isBalanced(String s){
+    	Stack<Character> Stack = new Stack<Character>();
+    	for(int i=0; i<s.length();i++){
+    		if(s.charAt(i) == LEFT_PAREN)
+    			Stack.push(LEFT_PAREN);
+    		if(s.charAt(i)== RIGHT_PAREN)
+    			Stack.push(LEFT_BRACE);
+    		if(s.charAt(i) == LEFT_BRACKET)
+    			Stack.push(LEFT_BRACKET);
+
+    		if(s.charAt(i)== RIGHT_PAREN){
+    			if(Stack.isEmpty())
+    				return false;
+    			if(Stack.pop()!= LEFT_PAREN)
+    				return false;
+    		}
+    		else if(s.charAt(i) == RIGHT_BRACE){
+    			if(Stack.isEmpty())
+    				return false;
+    			if(Stack.pop()!= LEFT_BRACE)
+    				return false;
+    		}
+    		else if(s.charAt(i) == RIGHT_BRACKET){
+    			if(Stack.isEmpty())
+    				return false;
+    			if(Stack.pop()!= LEFT_BRACKET)
+    				return false;
+    		}
+    	}
+    	return Stack.isEmpty();
+    }
+
 }
