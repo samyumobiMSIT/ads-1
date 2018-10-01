49b4778104a65e076eb6a3f26d1df121a0d914b0
samyumobiMSIT <samyumobi@msitprogram.net>
2018-10-01 19:20:37 +0530

    Selection Sort

--- assignments/m8/8.1 Selection Sort/Solution.java
+++ assignments/m8/8.1 Selection Sort/Solution.java
@@ -4,21 +4,20 @@ import java.util.Scanner;
 import java.util.Comparator;
 import  java.util.Arrays;
   
-public class Solution {  
-	 public static void sort(Comparable[] a) {
-	 	//sort and compares elements
-	        int n = a.length; 
-	        for (int i = 0; i < n; i++) {
-	            int min = i;
-	            for (int j = i+1; j < n; j++) {
-	                if (less(a[j], a[min])){
-	                	min = j;
-	                }      
-	                exch(a, i, min); 
-	              } 
-	        } 
-	       
-	    }
+public class Solution {
+	public static void sort(Comparable[] a) {
+	//sort and compares elements
+int n = a.length;
+for (int i = 0; i < n; i++) {
+	int min = i;
+	 for (int j = i+1; j < n; j++) {
+	 	if (less(a[j], a[min])){
+	 		min = j;
+	 	}
+	 	 exch(a, i, min);
+	 	}
+	 	}
+	 }
 	 
 	 // is v < w ?
 	    private static boolean less(final Comparable i,final Comparable j) {
@@ -30,16 +29,16 @@ public class Solution {
 	        a[i] = a[j];
 	        a[j] = swap;
 	    }
-	    
+
    public static void main(String args[])  
    {  
-    Scanner scan = new Scanner(System.in);        
-       String input = scan.nextLine();
-       for (int i = 0; i < Integer.parseInt(input); i++) {
-           String str = scan.nextLine();
-           String[] string = str.split(" ");
-           Solution.sort(string);
-         System.out.println(Arrays.toString(string));
+    Scanner scan = new Scanner(System.in);
+    String input = scan.nextLine();
+    for (int i = 0; i < Integer.parseInt(input); i++) {
+    	String str = scan.nextLine();
+    	String[] string = str.split(" ");
+    	Solution.sort(string);
+    	System.out.println(Arrays.toString(string));
          
        
        }
