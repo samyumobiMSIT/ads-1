fbdd2e5e1cdf9cf244a7c66e7e455c05d083d93f
samyumobiMSIT <samyumobi@msitprogram.net>
2018-10-01 19:29:32 +0530

    Hello

--- assignments/m8/8.1 Selection Sort/Solution.java
+++ assignments/m8/8.1 Selection Sort/Solution.java
@@ -9,8 +9,8 @@ public class Solution {
 int n = a.length;
 for (int i = 0; i < n; i++) {
 	int min = i;
-	 for (int j = i+1; j < n; j++) {
-	 	if (less(a[j], a[min])){
+	 for (int j = i+ 1; j < n; j++) {
+	 	if (less(a[j], a[min])) {
 	 		min = j;
 	 	}
 	 	 exch(a, i, min);
@@ -21,13 +21,14 @@ for (int i = 0; i < n; i++) {
 	  private static boolean less(final Comparable i,final Comparable j) {
 	  	return i.compareTo(j) < 0;
 	    }
-	     // exchange a[i] and a[j]
+	    // exchange a[i] and a[j]
 	    private static void exch(final Comparable[] a, final int i, final int j) {
 	        Comparable swap = a[i];
 	        a[i] = a[j];
 	        a[j] = swap;
 	    }
-	    public static void main(String args[]){
+	    //main method
+	    public static void main(String[] args) {
 	    Scanner scan = new Scanner(System.in);
     	String input = scan.nextLine();
     	for (int i = 0; i < Integer.parseInt(input); i++) {
@@ -35,8 +36,6 @@ for (int i = 0; i < n; i++) {
     	String[] string = str.split(" ");
     	Solution.sort(string);
     	System.out.println(Arrays.toString(string));
-         
-       
-       }
+    }
    } 
    } 
