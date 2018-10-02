0b50745227ea94c9aa770f59b4a7e3dee315986f
samyumobiMSIT <samyumobi@msitprogram.net>
2018-10-02 11:38:24 +0530

    Insert sort

--- assignments/m8/8.2 Team Ranking With Selection/Solution.java
+++ assignments/m8/8.2 Team Ranking With Selection/Solution.java
@@ -1,15 +1,15 @@
-import java.util.Scanner;  
+import java.util.Scanner;
 import  java.util.Arrays;
 //Solution class
-public class Solution {
-	public static void sort(final Comparable[] a) {
+public class Solution { //Solution class
+public static void sort(final Comparable[] a) {//sort method
 //sort and compares elements
 int n = a.length;
-for (int i = 0; i < n; i++ ) {
+for (int i = 0; i < n; i++ ){
 	for (int j = i; j > n; j--) {
-		if (less(a[j], a[j- 1])) {
-			exch(a, j, j- 1);
-		}else {
+		if (less(a[j], a[j - 1])) {
+			exch(a, j, j - 1);
+		} else {
 			break;
 		}
 	}
@@ -20,13 +20,13 @@ for (int i = 0; i < n; i++ ) {
 	  	return i.compareTo(j) < 0;
 	    }
 	    // exchange a[i] and a[j]
-	    private static void exch(final Comparable[] a, final int i, final int j) {
+	    private static void exch(final Comparable[] a, final int i, final int j) {//exch
 	        Comparable swap = a[i];
 	        a[i] = a[j];
 	        a[j] = swap;
 	    }
 //main method
-	    public static void main(String[] args) {
+	    public static final void main(String[] args) {
 	    Scanner scan = new Scanner(System.in);
     	String input = scan.nextLine();
     	for (int i = 0; i < Integer.parseInt(input); i++) {
@@ -36,4 +36,4 @@ for (int i = 0; i < n; i++ ) {
     	System.out.println(Arrays.toString(string));
     }
     }
-}    
+}
