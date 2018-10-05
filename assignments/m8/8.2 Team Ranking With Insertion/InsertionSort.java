3dba621f0944e33958540f42d3b2c2a368b7bc0d
samyumobiMSIT <samyumobi@msitprogram.net>
2018-10-05 09:03:24 +0530

    ads module

--- assignments/m8/8.2 Team Ranking With Insertion/InsertionSort.java
+++ assignments/m8/8.2 Team Ranking With Insertion/InsertionSort.java
@@ -10,7 +10,7 @@ class InsertionSort {
 	public static void sort(final Comparable[] a) {
 		int N = a.length;
 		for (int i = 0; i < N; i++) {
-			for (int j = i; j > 0; j--) {
+			for (int j = i + 1 ; j > 0; j--) {
 				if (less(a[j], a[j - 1])) {
 					exch(a, j, j - 1);
 				} else break;
