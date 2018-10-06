89a82b9c1a491fb69f5b0b52c2cbb6ab6467b22e
samyumobiMSIT <samyumobi@msitprogram.net>
2018-10-06 12:29:39 +0530

    "ads1_m12_a1: 3/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m12/Assignment-1/Solution.java
+++ assignments/m12/Assignment-1/Solution.java
@@ -2,21 +2,87 @@ import java.util.Scanner;
 import java.util.Arrays;
 import java.util.ArrayList;
 
-public class Solution{
+public class Solution {
+    Solution() {
+        //unused
+    }
 
-	ArrayList<Student> students= new ArrayList<>();
-	Scanner scan= new Scanner(System.in);
-	int noLines= Integer.parseInt(scan.nextLine());
-	int vacancies=Integer.parseInt(scan.nextLine());
-	int unreserved= Integer.parseInt(scan.nextLine());
-	int bc=Integer.parseInt(scan.nextLine());
-	int st=Integer.parseInt(scan.nextLine());
+/**Main
+Method **/
+public static void main(String[] args) {
+	Scanner s =  new Scanner(System.in);
+    int num = Integer.parseInt(s.nextLine());
+    Student[] students = new Student[num];
+    int vac = Integer.parseInt(s.nextLine());
+    int unres= Integer.parseInt(s.nextLine());
+    int bcvac = Integer.parseInt(s.nextLine());
+    int scvac = Integer.parseInt(s.nextLine());
+    int stvac = Integer.parseInt(s.nextLine());
+    for (int i = 0; i < num; i++) {
+    	String line = s.nextLine();
+    	String[] tokens = line.split(",");
+    	Student eachstudentdata = new Student(tokens[0], tokens[1],
+                    Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]),
+                    Integer.parseInt(tokens[4]), Integer.parseInt(tokens[5]), tokens[6]);
+            students[i] = eachstudentdata;
+        }
+        InsertionSort insertion = new InsertionSort();
+        insertion.sort(students);
+        print(students);
+        System.out.println();
+        FilterResults(students, vac, unres, bcvac, scvac, stvac);
+    }
 
-	while(scan.hasNext()){
-		String[] tokens=scan.nextLine().split(",");
-		System.out.println(Arrays.toString(tokens));
-		Student s=new Student(tokens[0],tokens[1],tokens[2],tokens[3],
-			tokens[4],tokens[5],tokens[6]);
-		students.add(s);
-	}
+/** Filter Results by 
+s3> s2> s1> younger 
+parameters: Student class info details in form of list
+vac- vacanises , unres- unreserved, categorys**/
+	 public static void FilterResults(Student[] list, int vac, int unres, int bc, int sc, int st) {
+        for (int i = 0; i < list.length && vac > 0; i++) {
+            if (unres > 0) { 
+            	/** check if there are any unreserved seats **/
+                System.out.println(list[i].sname + "," +
+                                   list[i].tmarks + "," +
+                                   list[i].rescat);
+                unres--;
+            } else if (list[i].rescat.equals("BC") || list[i].rescat.equals("SC") || list[i].rescat.equals("ST")) {
+                if (list[i].rescat.equals("BC") && bc > 0) { 
+                // reserve seats for BC category
+                    System.out.println(list[i].sname + "," +
+                                       list[i].tmarks + "," +
+                                       list[i].rescat);
+                    bc--;
+                } else if (list[i].rescat.equals("SC") && sc > 0) {
+                	  // reserve seats for SC category
+                    System.out.println(list[i].sname + "," +
+                                       list[i].tmarks + "," +
+                                       list[i].rescat);
+                    sc--;
+                } else if (list[i].rescat.equals("ST") && st > 0) {
+                	  // reserve seats for St category
+                    System.out.println(list[i].sname + "," +
+                                       list[i].tmarks + "," +
+                                       list[i].rescat);
+                    st--;
+                } else {
+                }
+            }
+        }
+        vac--;
+    }
+/** Print the Student List by iterating through
+ the student list array defined in Student.java **/
+	 public static void print(Student[] list) {
+        for (int i = 0; i < list.length; i++) {
+            System.out.println(list[i].sname + "," +
+                               list[i].tmarks + "," +
+                               list[i].rescat);
+        }
+    }
 }
+    
+
+
+
+
+
--- assignments/m12/Assignment-1/Student.java
+++ assignments/m12/Assignment-1/Student.java
@@ -1,31 +1,71 @@
-public class Student implements Comparable<Student> {
-
-	public Student() {
-	 String name,rc;
-	 int vacancies;
-	 int unreserved;
-	 int BC;
-	 int SC ;
-	 int ST;
-	 int m1,m2,m3,tmarks;
-	 int day,month,year;
-
-	 Student(String name,String dob,String m1,
-	 	String m2,String m3,String tm,String rc){
-	 	String[] d= dob.split("-");
-	 	this.day=Integer.parseInt(d[0]);
-	 	this.month=Integer.parseInt(d[1]);
-	 	this.year=Integer.parseInt(d[2]);
-	 	this.m1=Integer.parseInt(m1);
-	 	this.m2=Integer.parseInt(m2);
-	 	this.m3=Integer.parseInt(m3);
-	 	this.tmarks=Integer.parseInt(tmarks);
-	 	this.rc=rc;
-
-
-	 }
-		
-		
-	}
+/** Student 
+Class to take inputs in given test case format**/
+class Student implements Comparable<Student> {
+    String sname;
+    String bday;
+    int sm1;
+    int sm2;
+    int sm3;
+    int tmarks;
+    String rescat;
+    /** Student Method 
+    to display student details**/
+    Student(String name, String dob, int sub1, int sub2, int sub3, int total, String category) {
+        this.sname = name;
+        this.bday = dob;
+        this.sm1 = sub1;
+        this.sm2 = sub2;
+        this.sm3 = sub3;
+        this.tmarks = total;
+        this.rescat = category;
+    }
+    /** Split date time and month 
+    in date with '-'
+    **/
+    String[] splitDate() {
+        String[] splitdate = bday.split("-");
+        return splitdate;
+    }
+    /**Student class uses other object 
+    to compare every parameter**/
+    public int compareTo(Student other) {
+        if (this.tmarks > other.tmarks) {
+            return 1;
+        }
+        if (this.tmarks < other.tmarks) {
+            return -1;
+        }
+        if (this.sm3 > other.sm3) {
+            return 1;
+        }
+        if (this.sm3 < other.sm3) {
+            return -1;
+        }
+        if (this.sm2 > other.sm2) {
+            return 1;
+        }
+        if (this.sm2 < other.sm2) {
+            return -1;
+        }
+        if (Integer.parseInt(this.splitDate()[2]) > Integer.parseInt(other.splitDate()[2])) {
+            return 1;
+        }
+        if (Integer.parseInt(this.splitDate()[2]) < Integer.parseInt(other.splitDate()[2])) {
+            return -1;
+        }
+        if (Integer.parseInt(this.splitDate()[1]) > Integer.parseInt(other.splitDate()[1])) {
+            return 1;
+        }
+        if (Integer.parseInt(this.splitDate()[1]) < Integer.parseInt(other.splitDate()[1])) {
+            return -1;
+        }
+        if (Integer.parseInt(this.splitDate()[0]) > Integer.parseInt(other.splitDate()[0])) {
+            return 1;
+        }
+        if (Integer.parseInt(this.splitDate()[0]) < Integer.parseInt(other.splitDate()[0])) {
+            return -1;
+        }
+        return 0;
+    }
 
 }
