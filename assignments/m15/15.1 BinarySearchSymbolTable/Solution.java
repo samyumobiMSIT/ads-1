84ec010f35a0c5823784d10778ea4618400987b1
samyumobiMSIT <samyumobi@msitprogram.net>
2018-10-10 14:06:02 +0530

    "ads1_m15_a1: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ assignments/m15/15.1 BinarySearchSymbolTable/Solution.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Solution{
+	public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] input = sc.nextLine().split(" ");
+        BinarySearchST st = new BinarySearchST(input.length);
+        for (int i = 0; i < input.length; i++) {
+            st.put(input[i], i);
+        }
+        while (sc.hasNextLine()) {
+            String[] operation = sc.nextLine().split(" ");
+            switch (operation[0]) {
+            case "contains":
+                System.out.println(st.contains(operation[1]));
+                break;
+            case "get":
+                System.out.println(st.get(operation[1]));
+                break;
+            case "max":
+                System.out.println(st.max());
+                break;
+            case "floor":
+                System.out.println(st.floor(operation[1]));
+                break;
+            case "rank":
+                System.out.println(st.rank(operation[1]));
+                break;
+            case "deleteMin":
+                st.deleteMin();
+                break;
+            case "keys":
+                st.keys();
+                break;
+            default:
+                break;
+            }
+        }
+    }
+}
