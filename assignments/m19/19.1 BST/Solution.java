b8796ba9256427be262de7d3899a095b697f4a37
samyumobiMSIT <samyumobi@msitprogram.net>
2018-10-22 14:24:20 +0530

    "ads1_m19_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m19/19.1 BST/Solution.java
+++ assignments/m19/19.1 BST/Solution.java
@@ -1,174 +1,61 @@
-
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-import java.util.TreeMap;
-import java.util.Scanner;
-
-
-/**
- *  This implementation uses a balanced binary search tree. 
+/**.
+ * { item_description }
  */
-public class ST<Key extends Comparable<Key>, Value> implements Iterable<Key> {
-
-    private TreeMap<Key, Value> st;
-
-    /**
-     * Initializes an empty symbol table.
+import java.util.Scanner;
+/**.
+ * { item_description }
+ */
+public final class Solution {
+    /**.
+     * Constructs the object.
      */
-    public ST() {
-        st = new TreeMap<Key, Value>();
+    private Solution() {
+        /**.
+         * author: Samyuktha
+         */
     }
-
-
-    /**
-     * Returns the value associated with the given key.
-     * @param key the key
-     * @return the value associated with the given key if the key is in the symbol table
-     *     and <tt>null</tt> if the key is not in the symbol table
-     * @throws NullPointerException if <tt>key</tt> is <tt>null</tt>
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
      */
-    public Value get(Key key) {
-        if (key == null) throw new NullPointerException("called get() with null key");
-        return st.get(key);
-    }
-
-    /**
-     * Inserts the key-value pair into the symbol table, overwriting the old value
-     * with the new value if the key is already in the symbol table.
-     * If the value is <tt>null</tt>, this effectively deletes the key from the symbol table.
-     * @param key the key
-     * @param val the value
-     * @throws NullPointerException if <tt>key</tt> is <tt>null</tt>
-     */
-    public void put(Key key, Value val) {
-        if (key == null) throw new NullPointerException("called put() with null key");
-        if (val == null) st.remove(key);
-        else             st.put(key, val);
-    }
-
-    /**
-     * Removes the key and associated value from the symbol table
-     * (if the key is in the symbol table).
-     * @param key the key
-     * @throws NullPointerException if <tt>key</tt> is <tt>null</tt>
-     */
-    public void delete(Key key) {
-        if (key == null) throw new NullPointerException("called delete() with null key");
-        st.remove(key);
-    }
-
-    /**
-     * Does this symbol table contain the given key?
-     * @param key the key
-     * @return <tt>true</tt> if this symbol table contains <tt>key</tt> and
-     *     <tt>false</tt> otherwise
-     * @throws NullPointerException if <tt>key</tt> is <tt>null</tt>
-     */
-    public boolean contains(Key key) {
-        if (key == null) throw new NullPointerException("called contains() with null key");
-        return st.containsKey(key);
-    }
-
-    /**
-     * Returns the number of key-value pairs in this symbol table.
-     * @return the number of key-value pairs in this symbol table
-     */
-    public int size() {
-        return st.size();
-    }
-
-    /**
-     * Is this symbol table empty?
-     * @return <tt>true</tt> if this symbol table is empty and <tt>false</tt> otherwise
-     */
-    public boolean isEmpty() {
-        return size() == 0;
-    }
-
-    /**
-     * Returns all keys in the symbol table as an <tt>Iterable</tt>.
-     * To iterate over all of the keys in the symbol table named <tt>st</tt>,
-     * use the foreach notation: <tt>for (Key key : st.keys())</tt>.
-     * @return all keys in the sybol table as an <tt>Iterable</tt>
-     */
-    public Iterable<Key> keys() {
-        return st.keySet();
-    }
-
-    /**
-     * Returns all of the keys in the symbol table as an iterator.
-     * To iterate over all of the keys in a symbol table named <tt>st</tt>, use the
-     * foreach notation: <tt>for (Key key : st)</tt>.
-     * @deprecated Use {@link #keys} instead.
-     * This method is provided for backward compatibility with the version from
-     * <em>Introduction to Programming in Java: An Interdisciplinary Approach.</em>
-     * @return an iterator to all of the keys in the symbol table
-     */
-    @Deprecated
-    public Iterator<Key> iterator() {
-        return st.keySet().iterator();
-    }
-
-    /**
-     * Returns the smallest key in the symbol table.
-     * @return the smallest key in the symbol table
-     * @throws NoSuchElementException if the symbol table is empty
-     */
-    public Key min() {
-        if (isEmpty()) throw new NoSuchElementException("called min() with empty symbol table");
-        return st.firstKey();
-    }
-
-    /**
-     * Returns the largest key in the symbol table.
-     * @return the largest key in the symbol table
-     * @throws NoSuchElementException if the symbol table is empty
-     */
-    public Key max() {
-        if (isEmpty()) throw new NoSuchElementException("called max() with empty symbol table");
-        return st.lastKey();
-    }
-
-    /**
-     * Returns the smallest key in the symbol table greater than or equal to <tt>key</tt>.
-     * @return the smallest key in the symbol table greater than or equal to <tt>key</tt>
-     * @param key the key
-     * @throws NoSuchElementException if there is no such key
-     * @throws NullPointerException if <tt>key</tt> is <tt>null</tt>
-     */
-    public Key ceiling(Key key) {
-        if (key == null) throw new NullPointerException("called ceiling() with null key");
-        Key k = st.ceilingKey(key);
-        if (k == null) throw new NoSuchElementException("all keys are less than " + key);
-        return k;
-    }
-
-    /**
-     * Returns the largest key in the symbol table less than or equal to <tt>key</tt>.
-     * @return the largest key in the symbol table less than or equal to <tt>key</tt>
-     * @param key the key
-     * @throws NoSuchElementException if there is no such key
-     * @throws NullPointerException if <tt>key</tt> is <tt>null</tt>
-     */
-    public Key floor(Key key) {
-        if (key == null) throw new NullPointerException("called floor() with null key");
-        Key k = st.floorKey(key);
-        if (k == null) throw new NoSuchElementException("all keys are greater than " + key);
-        return k;
-    }
-
-    /**
-     * Unit tests the <tt>ST</tt> data type.
-     */
-    public static void main(String[] args) {
-     Scanner f=new Scanner(System.in);
-     ST<String, Integer> st = new ST<String, Integer>();
-       
-        for (int i = 0; !f.isEmpty(); i++) {
-            String key = f.readString();
-            st.put(key, i);
+     public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BinarySearchTree bst = new BinarySearchTree();
+        while (sc.hasNext()) {
+            String input = sc.nextLine();
+            String[] tokens = input.split(",");
+            // System.out.println(Arrays.toString(tokens));
+            switch (tokens[0]) {
+                case"put":
+                    bst.put(new Book(tokens[1], tokens[2],
+                     Float.parseFloat(tokens[2 + 1])),
+                      Integer.parseInt(tokens[2 + 2]));
+                    break;
+                case"get":
+                    System.out.println(bst.get(new Book(tokens[1], tokens[2],
+                     Float.parseFloat(tokens[2 + 1]))));
+                    break;
+                case"max":
+                    System.out.println(bst.max());
+                    break;
+                case"min":
+                    System.out.println(bst.min());
+                    break;
+                case"floor":
+                    System.out.println(bst.floor(new Book(tokens[1], tokens[2],
+                     Float.parseFloat(tokens[2 + 1]))));
+                    break;
+                case"ceiling":
+                    System.out.println(bst.ceil(new Book(tokens[1], tokens[2],
+                     Float.parseFloat(tokens[2 + 1]))));
+                    break;
+                case"select":
+                    System.out.println(bst.select(Integer.parseInt(tokens[1])));
+                    break;
+                default:
+                break;
+            }
         }
-        for (String s : st.keys())
-            System.out.println.println(s + " " + st.get(s));
     }
 }
