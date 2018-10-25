2738c623375317f89c93cad00f7ac60973adddf9
samyumobiMSIT <samyumobi@msitprogram.net>
2018-10-25 11:47:44 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m22/22.1 Ransom Note/Solution.java
+++ assignments/m22/22.1 Ransom Note/Solution.java
@@ -13,12 +13,9 @@ public class Solution {
     /**
      * Check if we have sufficient words in the magazine for the ransom note.
      */
-    static String sufficientWords(String[] magazine, String[] ransom) {
- 
-        // **** build magazine map ****
-         
-        HashMap<String, Integer> magMap = new HashMap<String, Integer>();
-         
+    static String sufficientWords(String[] magazine, String[] ransom) { 
+        // **** build magazine map ****         
+        HashMap<String, Integer> magMap = new HashMap<String, Integer>();         
         for (String word : magazine) {
             if (magMap.containsKey(word)) {
                 int count = magMap.get(word);
@@ -31,8 +28,7 @@ public class Solution {
  
         // **** build ransom map ****
          
-        HashMap<String, Integer> ransomMap = new HashMap<String, Integer>();
-         
+        HashMap<String, Integer> ransomMap = new HashMap<String, Integer>();         
         for (String word : ransom) {
             if (ransomMap.containsKey(word)) {
                 int count = ransomMap.get(word);
@@ -45,18 +41,11 @@ public class Solution {
          
         // **** compare words and counts ****
          
-        Iterator iter = ransomMap.entrySet().iterator();
-         
+        Iterator iter = ransomMap.entrySet().iterator();         
         while (iter.hasNext()) {
-             
-            // **** ****
-             
             Map.Entry<String, Integer> pair = (Map.Entry<String, Integer>)iter.next();
             String word = pair.getKey();
-            int count   = pair.getValue();
-             
-            // **** ****
-             
+            int count   = pair.getValue();             
             if (!magMap.containsKey(word)) {
                 return "No";
             } else {
@@ -65,9 +54,6 @@ public class Solution {
                 }
             }
         }
- 
-        // **** we have enough words ****
-         
         return "Yes";
     }
  
@@ -75,27 +61,20 @@ public class Solution {
      * Test code.
      */
     public static void main(String[] args) {
- 
-        // **** open scanner ****
-         
-        Scanner in  = new Scanner(System.in);
-         
-        // **** read parameters ****
-         
+
+        // **** open scanner ****         
+        Scanner in  = new Scanner(System.in);         
+        // **** read parameters ****         
         int m       = in.nextInt();
-        int n       = in.nextInt();
-         
-        // **** read words from the magazine ****
-         
+        int n       = in.nextInt();         
+        // **** read words from the magazine ****         
         String magazine[] = new String[m];
         for (int i = 0; i < m; i++){
             magazine[i] = in.next();
         }
-         
-        // **** read words for ransom note ****
-         
-        String ransom[] = new String[n];
-        for(int i = 0; i < n; i++){
+        // **** read words for ransom note ****         
+        String ransom [] = new String[n];
+        for (int i = 0; i < n; i++) {
             ransom[i] = in.next();
         }
          
@@ -108,7 +87,3 @@ public class Solution {
         System.out.println(sufficientWords(magazine, ransom));
     }
 }
-
-
-
- 
