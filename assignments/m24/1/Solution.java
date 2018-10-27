4f0d41042758a0fc3882cf6b668d6cc9418d293d
samyumobiMSIT <samyumobi@msitprogram.net>
2018-10-27 12:31:56 +0530

    "m24_a1_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m24/1/Solution.java
+++ assignments/m24/1/Solution.java
@@ -1,71 +1,109 @@
 import java.util.Scanner;
 /**
-*** Student class.
-**/
+ * Class for student.
+ */
 class Student {
-/**
-** roll no.
-**/
-    private String rno;
-   private String sname;
-   /** 
-    * sname.
-    **/
-   private double stotal;
-    /** 
-    * stotal1.
-    **/
+    /**
+     * rollnum.
+     */
+    private String rollno;
+    /**
+     * name.
+     */
+    private String name;
+    /**
+     * total marks.
+     */
+    private double total;
+    /**
+     * Constructs the object.
+     */
     Student() {
     }
-     /**
-     * 
+    /**
+     * Constructs the object.
+     *
      * @param      rollno1  The rollno
-     * @param      sname1    The name
-     * @param      stotal1  The total
+     * @param      name1    The name
+     * @param      total1  The total
      */
-    Student(final String rollno1, final String sname1, final double stotal1) {
-        this.rno = rollno1;
-        this.sname = sname1;
-        this.stotal = stotal1;
+    Student(final String rollno1, final String name1, final double total1) {
+        rollno = rollno1;
+        name = name1;
+        total = total1;
+    }
+    /**
+     * Gets the name.
+     *
+     * @return     The name.
+     */
+    public String getName() {
+        return name;
+    }
+    /**
+     * Gets the rollno.
+     *
+     * @return   The rollno.
+     */
+    public String gerRoll() {
+        return rollno;
+    }
+    /**
+     * Gets the total.
+     *
+     * @return     The total.
+     */
+    public Double getmar() {
+        return total;
     }
 }
 /**
-** Solution class.
-**/
-class Solution {
-/**
-** Main. 
-* @param String name(rollno)
-* @param Double totalmarks
-**/
-public static void main(final String[] args) {
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         Student s = new Student();
-        LinearProbingHashST<String, String> lp1 = new LinearProbingHashST();
-        LinearProbingHashST<String, Double> lp2 = new LinearProbingHashST();
+        LinearProbingHashST<String, String> lpht1 = new LinearProbingHashST();
+        LinearProbingHashST<String, Double> lpht2 = new LinearProbingHashST();
         int n = Integer.parseInt(sc.nextLine());
         for (int i = 0; i < n; i++) {
              String str = sc.nextLine();
              String[] tokens = str.split(",");
              s = new Student(tokens[0], tokens[1],
                 Double.parseDouble(tokens[2]));
-             lp1.put(tokens[0], tokens[1]);
-             lp2.put(tokens[0], Double.parseDouble(tokens[2]));
+             lpht1.put(tokens[0], tokens[1]);
+             lpht2.put(tokens[0], Double.parseDouble(tokens[2]));
             }
         int m = Integer.parseInt(sc.nextLine());
         for (int i = 0; i < m; i++) {
             String que = sc.nextLine();
             String[] query = que.split(" ");
-            if (!lp1.contains(query[1])) {
+            if (!lpht1.contains(query[1])) {
                 System.out.println("Student doesn't exists...");
                 continue;
             }
             switch (query[2]) {
                 case "1":
-                System.out.println(lp1.get(query[1]));
+                    System.out.println(lpht1.get(query[1]));
+
+                //System.out.println(lpht1.get(query[1]));
+                //lpht1.get(query[1]);
                 break;
                 case "2":
-                System.out.println(lp2.get(query[1]));
+                System.out.println(lpht2.get(query[1]));
+                //lpht2.get(query[2]);
                 break;
                 default:
                 break;
