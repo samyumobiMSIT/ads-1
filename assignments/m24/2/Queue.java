8357c482e3f6225d021d4df1b449b47720e6053f
samyumobiMSIT <samyumobi@msitprogram.net>
2018-10-27 12:29:29 +0530

    "m24_a2_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m24/2/Queue.java
+++ assignments/m24/2/Queue.java
@@ -1,30 +1,57 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-
-public class Queue<Item> implements Iterable<Item> {
-    private Node<Item> first;    // beginning of queue
-    private Node<Item> last;     // end of queue
-    private int n;               // number of elements on queue
-
-    // helper linked list class
+/**
+ * Queue class for list of items.
+ *
+ * @param      <Item>  The item
+ */
+class Queue<Item> implements Iterable<Item> {
+    /**
+     * { variable for beginning of queue }.
+     */
+    private Node<Item> first;
+    /**
+     * { variable for end of queue }.
+     */
+    private Node<Item> last;
+    /**
+     * { variable for number of elements on queue }.
+     */
+    private int n;
+    /**
+     * Class for node.
+     * helper linked list class.
+     *
+     * @param      <Item>  The item
+     */
     private static class Node<Item> {
+        /**
+         * { variable for item }.
+         */
         private Item item;
+        /**
+         * { variable for node next }.
+         */
         private Node<Item> next;
     }
-
     /**
+     * Constructs the object.
      * Initializes an empty queue.
      */
-    public Queue() {
+    Queue() {
         first = null;
         last  = null;
         n = 0;
     }
-
     /**
      * Returns true if this queue is empty.
      *
-     * @return {@code true} if this queue is empty; {@code false} otherwise
+     * Complexity :
+     *              Best Case : O(1)
+     *              Average Case : O(1)
+     *              Worst Case : O(1)
+     *
+     * @return     True if empty, False otherwise.
      */
     public boolean isEmpty() {
         return first == null;
@@ -32,56 +59,84 @@ public class Queue<Item> implements Iterable<Item> {
 
     /**
      * Returns the number of items in this queue.
-     *
-     * @return the number of items in this queue
+     * Complexity :
+     *              Best Case : O(1)
+     *              Average Case : O(1)
+     *              Worst Case : O(1)
+     * @return     { int value }
      */
     public int size() {
         return n;
     }
 
     /**
-     * Returns the item least recently added to this queue.
-     *
-     * @return the item least recently added to this queue
-     * @throws NoSuchElementException if this queue is empty
+     * { Returns the item least recently added to this queue }.
+     * Complexity :
+     *              Best Case : O(1)
+     *              Average Case : O(1)
+     *              Worst Case : O(1)
+     * @return     { the item least recently added to this queue }
      */
     public Item peek() {
-        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+        if (isEmpty()) {
+            throw new NoSuchElementException("Queue underflow");
+        }
         return first.item;
     }
 
     /**
-     * Adds the item to this queue.
-     *
-     * @param  item the item to add
+     * { Adds the item to this queue }.
+     * Complexity :
+     *              Best Case : O(1)
+     *              Average Case : O(1)
+     *              Worst Case : O(1)
+     * @param      item  The item
      */
-    public void enqueue(Item item) {
+    public void enqueue(final Item item) {
         Node<Item> oldlast = last;
         last = new Node<Item>();
         last.item = item;
         last.next = null;
-        if (isEmpty()) first = last;
-        else           oldlast.next = last;
+        if (isEmpty()) {
+            first = last;
+        } else {
+            oldlast.next = last;
+        }
         n++;
     }
 
     /**
-     * Removes and returns the item on this queue that was least recently added.
+     * { Removes and returns the item on this queue
+     *   that was least recently added }.
      *
-     * @return the item on this queue that was least recently added
+     * Complexity :
+     *              Best Case : O(1)
+     *              Average Case : O(1)
+     *              Worst Case : O(1)
+     *
+     * @return     { the item on this queue that was least recently added }
      * @throws NoSuchElementException if this queue is empty
      */
     public Item dequeue() {
-        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+        if (isEmpty()) {
+            throw new NoSuchElementException("Queue underflow");
+        }
         Item item = first.item;
         first = first.next;
         n--;
-        if (isEmpty()) last = null;   // to avoid loitering
+        if (isEmpty()) {
+            last = null;
+        }
         return item;
     }
 
     /**
      * Returns a string representation of this queue.
+     *
+     * Complexity :
+     *              Best Case : O(1)
+     *              Average Case : O(1)
+     *              Worst Case : O(1)
      *
      * @return the sequence of items in FIFO order, separated by spaces
      */
@@ -92,34 +147,68 @@ public class Queue<Item> implements Iterable<Item> {
             s.append(' ');
         }
         return s.toString();
-    } 
-
-    /**
-     * Returns an iterator that iterates over the items in this queue in FIFO order.
-     *
-     * @return an iterator that iterates over the items in this queue in FIFO order
-     */
-    public Iterator<Item> iterator()  {
-        return new ListIterator<Item>(first);  
     }
 
-    // an iterator, doesn't implement remove() since it's optional
+    /**
+     * { Returns an iterator that iterates over the
+     *   items in this queue in FIFO order }.
+     *
+     * Complexity :
+     *              Best Case : O(1)
+     *              Average Case : O(1)
+     *              Worst Case : O(1)
+     *
+     * @return     { an iterator that iterates over the items
+     *               in this queue in FIFO order}
+     */
+    public Iterator<Item> iterator()  {
+        return new ListIterator<Item>(first);
+    }
+
+    /**
+     * Class for list iterator.
+     *
+     * @param      <Item>  The item
+     */
     private class ListIterator<Item> implements Iterator<Item> {
+        /**
+         * { variable for current node }.
+         */
         private Node<Item> current;
-
-        public ListIterator(Node<Item> first) {
-            current = first;
+        /**
+         * Constructs the object.
+         *
+         * @param      first1  The first
+         */
+        ListIterator(final Node<Item> first1) {
+            current = first1;
         }
-
-        public boolean hasNext()  { return current != null;                     }
-        public void remove()      { throw new UnsupportedOperationException();  }
-
+        /**
+         * Determines if it has next.
+         *
+         * @return     True if has next, False otherwise.
+         */
+        public boolean hasNext() {
+            return current != null;
+        }
+        /**
+         * { function for remove }.
+         */
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+        /**
+         * { function for next }.
+         *
+         * @return     { description_of_the_return_value }
+         */
         public Item next() {
-            if (!hasNext()) throw new NoSuchElementException();
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
             Item item = current.item;
-            current = current.next; 
+            current = current.next;
             return item;
         }
     }
 }
-
