fdad525017e53e823af28cf1960cf8aaf8fa6644
samyumobiMSIT <samyumobi@msitprogram.net>
2018-10-27 11:48:33 +0530

    "m24_a1_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m24/1/Solution.java
+++ assignments/m24/1/Solution.java
@@ -6,12 +6,12 @@ class Student {
 /**
 ** roll no.
 **/
-    private String rollno;
-   private String name;
+    private String rno;
+   private String sname;
    /** 
     * name.
     **/
-   private double total;
+   private double stotal;
     /** 
     * total.
     **/
@@ -23,10 +23,10 @@ class Student {
      * @param      name1    The name
      * @param      total1  The total
      */
-    Student(final String rollno1, final String sname, final double stotal) {
-        this.rollno = rollno1;
-        this.name = sname;
-        this.total = stotal;
+    Student(final String rollno1, final String sname1, final double stotal1) {
+        this.rno = rollno1;
+        this.sname = sname1;
+        this.stotal = stotal1;
     }
 }
 /**
@@ -40,33 +40,33 @@ class Solution {
 public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         Student s = new Student();
-        LinearProbingHashST<String, String> lpht1 = new LinearProbingHashST();
-        LinearProbingHashST<String, Double> lpht2 = new LinearProbingHashST();
+        LinearProbingHashST<String, String> lp1 = new LinearProbingHashST();
+        LinearProbingHashST<String, Double> lp2 = new LinearProbingHashST();
         int n = Integer.parseInt(sc.nextLine());
         for (int i = 0; i < n; i++) {
              String str = sc.nextLine();
              String[] tokens = str.split(",");
              s = new Student(tokens[0], tokens[1],
                 Double.parseDouble(tokens[2]));
-             lpht1.put(tokens[0], tokens[1]);
-             lpht2.put(tokens[0], Double.parseDouble(tokens[2]));
+             lp1.put(tokens[0], tokens[1]);
+             lp2.put(tokens[0], Double.parseDouble(tokens[2]));
             }
         int m = Integer.parseInt(sc.nextLine());
         for (int i = 0; i < m; i++) {
             String que = sc.nextLine();
             String[] query = que.split(" ");
-            if (!lpht1.contains(query[1])) {
+            if (!lp1.contains(query[1])) {
                 System.out.println("Student doesn't exists...");
                 continue;
             }
             switch (query[2]) {
                 case "1":
-                    System.out.println(lpht1.get(query[1]));
+                    System.out.println(lp1.get(query[1]));
 
                 
                 break;
                 case "2":
-                System.out.println(lpht2.get(query[1]));
+                System.out.println(lp2.get(query[1]));
                 
                 break;
                 default:
