611fd352a908e24a245dbdc492b228e7a6a57c99
samyumobiMSIT <samyumobi@msitprogram.net>
2018-10-27 12:43:36 +0530

    "m24_a2_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m24/2/Solution.java
+++ assignments/m24/2/Solution.java
@@ -1,12 +1,78 @@
 import java.util.Scanner;
+
+/**
+** Solution class.
+**/
+class Solution {
+/**
+** Main. 
+** @param args String , Double
+**/
+public static void main(final String[] args) {
+    Scanner scan = new Scanner(System.in);
+        int datalen = Integer.parseInt(scan.nextLine());
+        RedBlackBST<Student, Integer> student = new RedBlackBST<>();
+        Student s = null;
+        while (datalen > 0) {
+            String[] line = scan.nextLine().split(",");
+            s = new Student(line[1], line[2], line[0]);
+            student.put(s, Integer.parseInt(line[0]));
+
+            datalen--;
+        }
+
+        int testcases = Integer.parseInt(scan.nextLine());
+        while (testcases > 0) {
+            String[] tokens = scan.nextLine().split(" ");
+            if (tokens[0].equals("BE")) {
+                //int key = Integer.parseInt(tokens[1]);
+                double lower = Double.parseDouble(tokens[1]);
+                double upper = Double.parseDouble(tokens[2]);
+                //System.out.println("here");
+                for (Student each : student.keys()) {
+                    //System.out.println("hi");
+                    //System.out.println(each);
+                    if (each.getMarks() >= lower
+                            && each.getMarks() <= upper) {
+                        System.out.println(
+                            each.getName());
+                    }
+                }
+                //System.out.println("----");
+
+            } else if (tokens[0].equals("LE")) {
+                double limit = Double.parseDouble(tokens[1]);
+                for (Student each : student.keys()) {
+                    if (each.getMarks() <= limit) {
+                        System.out.println(
+                            each.getName());
+                    }
+                }
+                //System.out.println("----");
+            } else if (tokens[0].equals("GE")) {
+                double limit = Double.parseDouble(tokens[1]);
+                for (Student each : student.keys()) {
+                    if (each.getMarks() >= limit) {
+                        System.out.println(
+                            each.getName());
+                    }
+                }
+                //System.out.println("----");
+            }
+
+            testcases--;
+        }
+
+    }
+}
 /**
 *** Student class.
 **/
-class Student {
+class Student implements Comparable<Student> {
 /**
 ** roll no.
 **/
-    private String rollno;
+    private int rollno;
    private String name;
    /** 
     * name.
@@ -17,55 +83,57 @@ class Student {
     **/
     Student() {
     }
-    Student(final String rollno1, final String sname, final double stotal) {
-        this.rollno = rollno1;
+    Student(final String rollno1, final String sname, final String stotal) {
+        this.rollno = Integer.parseInt(rollno1);
         this.name = sname;
-        this.total = stotal;
+        this.total = Double.parseDouble(stotal);
     }
-}
-/**
-** Solution class.
-**/
-class Solution {
-/**
-** Main. 
-** @param args String , Double
-**/
-public static void main(final String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Student s = new Student();
-        LinearProbingHashST<String, String> lpht1 = new LinearProbingHashST();
-        LinearProbingHashST<String, Double> lpht2 = new LinearProbingHashST();
-        int n = Integer.parseInt(sc.nextLine());
-        for (int i = 0; i < n; i++) {
-             String str = sc.nextLine();
-             String[] tokens = str.split(",");
-             s = new Student(tokens[0], tokens[1],
-                Double.parseDouble(tokens[2]));
-             lpht1.put(tokens[0], tokens[1]);
-             lpht2.put(tokens[0], Double.parseDouble(tokens[2]));
-            }
-        int m = Integer.parseInt(sc.nextLine());
-        for (int i = 0; i < m; i++) {
-            String que = sc.nextLine();
-            String[] query = que.split(" ");
-            if (!lpht1.contains(query[1])) {
-                System.out.println("Student not present");
-                continue;
-            }
-            switch (query[2]) {
-                case "1":
-                    System.out.println(lpht1.get(query[1]));
-
-                
-                break;
-                case "2":
-                System.out.println(lpht2.get(query[1]));
-                
-                break;
-                default:
-                break;
-            }
+    /**
+     * compareTo function.
+     *
+     * @param      other  The other
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int compareTo(final Student other) {
+        if (this.total > other.total) {
+            return 1;
         }
+        if (this.total < other.total) {
+            return -1;
+        }
+        if (this.rollno > other.rollno) {
+            return 1;
+        }
+        if (this.rollno < other.rollno) {
+            return -1;
+        }
+
+        return 0;
+    }
+    /**
+     * Gets the name.
+     *
+     * @return     The name.
+     */
+    public String getName() {
+        return name;
+    }
+    /**
+     * Gets the marks.
+     *
+     * @return     The marks.
+     */
+    public Double getMarks() {
+        return total;
+    }
+    /**
+     * Gets the roll.
+     *
+     * @return     The roll.
+     */
+    public Integer getRoll() {
+        return rollno;
     }
 }
+
