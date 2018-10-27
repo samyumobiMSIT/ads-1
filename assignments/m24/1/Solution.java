3d49869e65baae208018872ca4d55e2adc0b1d07
samyumobiMSIT <samyumobi@msitprogram.net>
2018-10-27 11:13:26 +0530

    "m24_a1_1: 2/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m24/1/Solution.java
+++ assignments/m24/1/Solution.java
@@ -1,23 +1,27 @@
 import java.util.*;
 class Student {
+    /**
+    * params: 
+    * rollno,name, total
+    */
     String rollno;
    private String name;
    private double total;
     Student() {
     }
-    Student(final String rollno,String name, double total) {
+    Student(final String rollno, final String name, final double total) {
         this.rollno = rollno;
         this.name = name;
         this.total = total;
     }
 }
 /**
-** Solution class
+** Solution class.
 **/
 class Solution {
-    Solution() {
-
-    }
+    /**
+    ** Main. 
+    **/
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Student s = new Student();
@@ -25,9 +29,9 @@ class Solution {
         LinearProbingHashST<String, Double> lpht2 = new LinearProbingHashST();
         int n = Integer.parseInt(sc.nextLine());
         for (int i = 0; i < n; i++) {
-             String str = sc.nextLine();
-             String[] tokens = str.split(",");
-             s = new Student(tokens[0], tokens[1], 
+            String str = sc.nextLine();
+            String[] tokens = str.split(",");
+            s = new Student(tokens[0], tokens[1], 
                 Double.parseDouble(tokens[2]));
              lpht1.put(tokens[0], tokens[1]);
              lpht2.put(tokens[0], Double.parseDouble(tokens[2]));
