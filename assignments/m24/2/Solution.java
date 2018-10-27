0d50dbafbb4aa9c358eed52f9c9cbfb1450168db
samyumobiMSIT <samyumobi@msitprogram.net>
2018-10-27 12:57:23 +0530

    "m24_a2_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m24/2/Solution.java
+++ assignments/m24/2/Solution.java
@@ -1,116 +1,31 @@
 import java.util.Scanner;
-import java.util.NoSuchElementException;
-
 /**
-** Solution class.
-**/
-class Solution {
-/**
-** Main. 
-** @param args String , Double
-**/
-public static void main(final String[] args) {
-    Scanner scan = new Scanner(System.in);
-        int datalen = Integer.parseInt(scan.nextLine());
-        RedBlackBST<Student, Integer> student = new RedBlackBST<>();
-        Student s = null;
-        while (datalen > 0) {
-            String[] line = scan.nextLine().split(",");
-            s = new Student(line[1], line[2], line[0]);
-            student.put(s, Integer.parseInt(line[0]));
-
-            datalen--;
-        }
-
-        int testcases = Integer.parseInt(scan.nextLine());
-        while (testcases > 0) {
-            String[] tokens = scan.nextLine().split(" ");
-            if (tokens[0].equals("BE")) {
-                //int key = Integer.parseInt(tokens[1]);
-                double lower = Double.parseDouble(tokens[1]);
-                double upper = Double.parseDouble(tokens[2]);
-                //System.out.println("here");
-                for (Student each : student.keys()) {
-                    //System.out.println("hi");
-                    //System.out.println(each);
-                    if (each.getMarks() >= lower
-                            && each.getMarks() <= upper) {
-                        System.out.println(
-                            each.getName());
-                    }
-                }
-                //System.out.println("----");
-
-            } else if (tokens[0].equals("LE")) {
-                double limit = Double.parseDouble(tokens[1]);
-                for (Student each : student.keys()) {
-                    if (each.getMarks() <= limit) {
-                        System.out.println(
-                            each.getName());
-                    }
-                }
-                //System.out.println("----");
-            } else if (tokens[0].equals("GE")) {
-                double limit = Double.parseDouble(tokens[1]);
-                for (Student each : student.keys()) {
-                    if (each.getMarks() >= limit) {
-                        System.out.println(
-                            each.getName());
-                    }
-                }
-                //System.out.println("----");
-            }
-
-            testcases--;
-        }
-
-    }
-}
-/**
-*** Student class.
-**/
+ * Class for student data.
+ */
 class Student implements Comparable<Student> {
-/**
-** roll no.
-**/
-    private int rollno;
-   private String name;
-   /** 
-    * name.
-    **/
-   private double total;
-    /** 
-    * total.
-    **/
-    Student() {
-    }
-    Student(final String rollno1, final String sname, final String stotal) {
-        this.rollno = Integer.parseInt(rollno1);
-        this.name = sname;
-        this.total = Double.parseDouble(stotal);
-    }
     /**
-     * compareTo function.
-     *
-     * @param      other  The other
-     *
-     * @return     { description_of_the_return_value }
+     * declaration of variable.
      */
-    public int compareTo(final Student other) {
-        if (this.total > other.total) {
-            return 1;
-        }
-        if (this.total < other.total) {
-            return -1;
-        }
-        if (this.rollno > other.rollno) {
-            return 1;
-        }
-        if (this.rollno < other.rollno) {
-            return -1;
-        }
-
-        return 0;
+    private String name;
+    /**
+     * declaration of variable.
+     */
+    private double total;
+    /**
+     * declaration of variable.
+     */
+    private int rollno;
+    /**
+     * Constructs the object.
+     *
+     * @param      n   The namee
+     * @param      t  The totall
+     * @param      r The rollnoo
+     */
+    Student(final String n, final String t, final String r) {
+        this.name  = n;
+        this.total = Double.parseDouble(t);
+        this.rollno = Integer.parseInt(r);
     }
     /**
      * Gets the name.
@@ -121,20 +36,104 @@ class Student implements Comparable<Student> {
         return name;
     }
     /**
-     * Gets the marks.
+     * Gets the total.
      *
-     * @return     The marks.
+     * @return     The total.
      */
-    public Double getMarks() {
+    public double getTotal() {
         return total;
     }
     /**
-     * Gets the roll.
+     * Gets the roll no.
      *
-     * @return     The roll.
+     * @return     The roll no.
      */
-    public Integer getRoll() {
+    public int getRollNo() {
         return rollno;
     }
+    /**
+     * compare To.
+     *
+     * @param      that  The that
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int compareTo(final Student that) {
+        if (this.total > that.total) {
+            return 1;
+        }
+        if (this.total < that.total) {
+            return -1;
+        }
+        // if (this.name.compareTo(that.name) > 0) {
+        //     return 1;
+        // }
+        // if (this.name.compareTo(that.name) < 0) {
+        //     return -1;
+        // }
+        if (this.rollno > that.rollno) {
+            return 1;
+        }
+        if (this.rollno < that.rollno) {
+            return -1;
+        }
+        return 0;
+    }
 }
-
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //empty constructor.
+    }
+    /**
+     * Client program.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int input = Integer.parseInt(scan.nextLine());
+        RedBlackBST<Student, Integer> student = new RedBlackBST<>();
+        Student s = null;
+        while (input > 0) {
+            String[] num = scan.nextLine().split(",");
+            s = new Student(num[1], num[2], num[0]);
+            student.put(s, Integer.parseInt(num[0]));
+            input--;
+        }
+        int testcases = Integer.parseInt(scan.nextLine());
+        while (testcases > 0) {
+            String[] tokens = scan.nextLine().split(" ");
+            if (tokens[0].equals("BE")) {
+                //int key = Integer.parseInt(tokens[1]);
+                double less = Double.parseDouble(tokens[1]);
+                double greater = Double.parseDouble(tokens[2]);
+                for (Student each : student.keys()) {
+                    if (each.getTotal() >= less && each.getTotal() <= greater) {
+                        System.out.println(each.getName());
+                    }
+                }
+            } else if (tokens[0].equals("LE")) {
+                double value = Double.parseDouble(tokens[1]);
+                for (Student each : student.keys()) {
+                    if (each.getTotal() <= value) {
+                        System.out.println(each.getName());
+                    }
+                }
+            } else if (tokens[0].equals("GE")) {
+                double value = Double.parseDouble(tokens[1]);
+                for (Student each : student.keys()) {
+                    if (each.getTotal() >= value) {
+                        System.out.println(each.getName());
+                    }
+                }
+            }
+            testcases--;
+        }
+    }
+}
